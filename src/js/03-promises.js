4abe634946c4fb9cfe7d87e80b83b7fdcfb9d66f
lubkepatryk <lubke.patryk@gmail.com>
2023-03-12 23:27:05 +0100

    task 3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const delayInput = document.querySelector('input[name="delay"]');
+  const stepInput = document.querySelector('input[name="step"]');
+  const amountInput = document.querySelector('input[name="amount"]');
+
+  const firstDelay = parseInt(delayInput.value);
+  const delayStep = parseInt(stepInput.value);
+  const amount = parseInt(amountInput.value);
+
+  let currentDay = firstDelay;
+
+  for (let i = 0; i <= amount; i++) {
+    createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        Notify.success('✅ Fulfilled promise ${position} in ${delay}ms');
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
+      });
   }
+});
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
