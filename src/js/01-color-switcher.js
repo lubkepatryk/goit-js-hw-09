292902646d6b4b549d8fc2c53ab46b2e8728cdb9
lubkepatryk <lubke.patryk@gmail.com>
2023-03-12 13:55:31 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+}
+
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+});
+
+stopBtn.addEventListener('click', () => {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+});
